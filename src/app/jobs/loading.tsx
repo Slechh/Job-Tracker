772470308107178e1488e5499163fea07ab4ac38
Slechh <ijopjojokp@gmail.com>
2026-06-29 13:34:03 +0300

    Add Loaging Page For Jobs List

---
+++ src/app/jobs/loading.tsx
@@ -0,0 +1,12 @@
+export default function JobsLoading() {
+  return (
+    <main className="py-4 px-4">
+      <div className="max-w-5xl mx-auto flex flex-col gap-3">
+        <h1 className="font-bold text-3xl">Jobs</h1>
+        <div className="flex-1 justify-center items-center">
+          <p className="mx-auto">Loading Jobs...</p>
+        </div>
+      </div>
+    </main>
+  );
+}
