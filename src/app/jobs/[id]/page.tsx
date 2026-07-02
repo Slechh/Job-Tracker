95a70d6fcd0d9798e7442d882489c97e3a3ac49d
Slechh <ijopjojokp@gmail.com>
2026-07-02 16:08:42 +0300

    Add Card When U Click On Her

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -1,3 +1,7 @@
+import { JobCard } from "@/components/jobs/JobCard";
+import { prisma } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
 type JobInfoPageProps = {
   params: Promise<{
     id: string;
@@ -7,10 +11,20 @@ type JobInfoPageProps = {
 export default async function JobInfoPage({ params }: JobInfoPageProps) {
   const { id } = await params;
 
+  const job = await prisma.job.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!job) {
+    notFound();
+  }
+
   return (
     <main className="px-4 py-8">
       <div className="max-w-5xl mx-auto">
-        <h1>Job Details</h1>
+        <JobCard job={job} />
       </div>
     </main>
   );
